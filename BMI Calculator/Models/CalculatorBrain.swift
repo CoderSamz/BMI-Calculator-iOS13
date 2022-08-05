349a11900244f943241381aa69d211240e7462da
CoderSamz <codersamz-dev@outlook.com>
2022-08-05 23:35:17 +0800

    129. Use Optionals in Practice and Add the Finishing Touches with Colour Literals 在练习中使用可选值，和 Colour Literals

--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -8,17 +8,35 @@
     
 
 import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmi: Float = 0.0
+    var bmi: BMI?
     
     mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / pow(height, 2)
+        let bmiValue = weight / pow(height, 2)
+    
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, adivce: "Eat more pies!", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1) )
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, adivce: "Fit as a fiddle!", color: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1) )
+        } else {
+            bmi = BMI(value: bmiValue, adivce: "Eat less pies!", color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1) )
+        }
+        
     }
     
     func getBMIValue() -> String {
-        let bmiTo1DecimalPlace = String(format: "%.1f", bmi)
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiTo1DecimalPlace
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.adivce ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.white
     }
 }
